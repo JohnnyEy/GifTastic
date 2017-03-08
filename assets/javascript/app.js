d898976b00852aa340aa9b4ba029cf67be57cbc7
Ashley MacWhirter <ashleymacwhirter@Ashleys-MacBook-Pro.local>
2017-03-08 00:08:26 -0500

    fully functional, basic styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,25 +18,54 @@ $(document).ready(function() {
         }).done(function(response) {
 
             //empty sports div so new selection appends to emtpy div - do not want previous searches listed
+
             $("#sports").empty();
 
             var results = response.data;
 
+            //user for loop to grab the rating information and appropriate gif for button clicked into its own div to keep information together
+
             for (var i = 0; i < results.length; i++) {
-                var sportDiv = $("<div class='sport'>");
+                var sportDiv = $("<div class='userSport'>");
+
+                //make variable for rating for clean appending
 
                 var rating = results[i].rating;
                 var pRate = $("<p>").text("Rating: " + rating);
 
-                var gifURL = results[i].images.fixed_height.url;
-                var gif = $("<img>").attr("src", gifURL);
+                //make variables for still url and animated url for clean build
+
+                var urlStill = results[i].images.fixed_height_still.url;
+                var urlPlay = results[i].images.fixed_height.url;
+
+                //gif needs still source to load, and data attributes to store the still and animated gifs for pausing function
+
+                var gif = $("<img>").addClass("gif").attr("src", urlStill).attr("data-still", urlStill).attr("data-animate", urlPlay).attr("data-state", "still");
+
+                //append the gif and rating to the new div created during for loop
 
                 sportDiv.append(gif);
                 sportDiv.append(pRate);
 
+                //append all for loop created divs to the DOM
 
                 $("#sports").append(sportDiv);
             }
+
+            //on click of gif still image, gif will play. When clicked again, gif will pause.
+
+            $(".gif").on("click", function() {
+                var state = $(this).attr("data-state");
+
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+
+            });
         });
 
     }
@@ -72,13 +101,13 @@ $(document).ready(function() {
 
         //push input to original topics array and then rerun render of buttons to show newly added button.
         topics.push(sport);
+            $("#sport-input").val(" ");
         renderButtons();
     });
 
+
     //on click entire document to cover all elements named "sport" and run display function
     $(document).on("click", ".sport", displayInfo);
-
-    //on click of gif still image, gif will play. When clicked again, gif will pause.
 
     //run function to display all buttons on startup
     renderButtons();
